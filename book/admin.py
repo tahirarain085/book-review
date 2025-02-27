92131fcc04b743a9830e11a8f9418b2840529f26
tahirarain085 <tahirarain085@gmail.com>
2025-02-27 22:33:12 +0500

    updated with admin and Model Forms

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from  .models import Book
 
 # Register your models here.
+@admin.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Book._meta.fields]
